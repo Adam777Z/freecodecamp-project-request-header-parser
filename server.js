a1c3e00edd56f9594302a8a0c3d52c5461377681
Glitch (accidental-okra) <none>
2018-12-07 16:51:44 +0000

    Updated with Glitch

--- server.js
+++ server.js
@@ -30,3 +30,14 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+app.enable('trust proxy');
+
+app.get('/api/whoami', function (req, res) {
+  res.json({
+    ipaddress: req['ip'],
+    language: req['headers']['accept-language'],
+    software: req['headers']['user-agent']
+  });
+});
