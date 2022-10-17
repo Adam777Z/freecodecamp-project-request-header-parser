8e129b2f189cf9a7b7de596a05b06b08951fb727
Adam777 <Adam777-5867@outlook.com>
2022-10-17 19:01:37 +0000

    Update

--- server.js
+++ server.js
@@ -8,36 +8,32 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200})); // some legacy browsers choke on 204
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // https://expressjs.com/en/starter/static-files.html
 app.use('/public', express.static(process.cwd() + '/public'));
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+	res.sendFile(__dirname + '/views/index.html');
 });
-
 
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
-  return res.json({greeting: 'hello API'});
+	return res.json({ greeting: 'hello API' });
 });
-
-
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+	console.log('Your app is listening on port ' + listener.address().port);
 });
-
 
 app.enable('trust proxy');
 
 app.get('/api/whoami', function (req, res) {
-  return res.json({
-    ipaddress: req['ip'],
-    language: req['headers']['accept-language'],
-    software: req['headers']['user-agent']
-  });
+	return res.json({
+		ipaddress: req['ip'],
+		language: req['headers']['accept-language'],
+		software: req['headers']['user-agent']
+	});
 });
